4c0a2b693bd4f9d9bead17489af6b47844a49a84
thejennjenn <jenniferhuynh@Jennifers-MacBook-Pro.local>
2016-10-02 23:13:46 -0700

    update latest version of redesign

--- js/javascript.js
+++ js/javascript.js
@@ -1,42 +1,18 @@
 $(document).ready(function() {
- 
-
-
-/*! 
-$(document).ready(function() {
-    
-    
-    $(window).scroll( function(){
-    
-     
-        $('.fadeThis').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-           
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'}, 1000);
-                    
-            }
-            
-        }); 
-    
-    });
-    
-});
-
-*/
 
 
 $(document).ready(function(){
-
-/*! Fades in page on load */
-$('body').css('display', 'none');
-$('body').fadeIn(700);
-
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
 });
+
+
+
+
 
 
 
